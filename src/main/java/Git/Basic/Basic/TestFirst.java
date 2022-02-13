2cb4ff63ed915026ca7c291eb3117a445555dcee
Aleksandar Korac <aleksandar.korac@yahoo.com>
2022-02-13 02:42:27 +0100

    Add a new Class TestFirst

---
+++ src/main/java/Git/Basic/Basic/TestFirst.java
@@ -0,0 +1,11 @@
+package Git.Basic.Basic;
+
+public class TestFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test 1 class");
+
+	}
+
+}
