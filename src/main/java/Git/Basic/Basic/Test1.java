301db66f2fe5c69d2341d5aa135034f1c64dda4b
Aleksandar Korac <aleksandar.korac@yahoo.com>
2022-02-13 01:41:24 +0100

    Add a new class Test1

---
+++ src/main/java/Git/Basic/Basic/Test1.java
@@ -0,0 +1,14 @@
+package Git.Basic.Basic;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test 1 class");
+	}
+
+	public void b() {
+		System.out.println("b");
+	}
+	
+}
