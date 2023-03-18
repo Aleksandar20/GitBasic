8c3dd6e488da6d1cc518e133150cdc40641578b9
Aleksandar <aleksandar.korac752@gmail.com>
2023-03-18 15:38:07 +0100

    add function qaaa

--- src/main/java/Git/Basic/Basic/Test1.java
+++ src/main/java/Git/Basic/Basic/Test1.java
@@ -2,107 +2,171 @@ package Git.Basic.Basic;
 
 public class Test1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+
 		// TODO Auto-generated method stub
 		System.out.println("Test 1 class");
+
 	}
 
-	public void b() {
+	public void b()
+	{
+
 		System.out.println("b");
+
 	}
-	
-	public void d() {
+
+	public void d()
+	{
+
 		System.out.println("d");
-		
+
 	}
-	public void c() {
-		//commentar 1
+
+	public void c()
+	{
+
+		// commentar 1
 		System.out.println("c");
-		
+
 	}
 
-	public void g() {
-		//commentar 1
-			System.out.println("g");
+	public void g()
+	{
+
+		// commentar 1
+		System.out.println("g");
+
 	}
 
-	public void f() {
-		//commentar f
+	public void f()
+	{
+
+		// commentar f
 		System.out.println("f");
-		
+
 	}
-	public void z() {
-		//commentar z
+
+	public void z()
+	{
+
+		// commentar z
 		System.out.println("z");
 
-		
 	}
-	public void h() {
-		//commentar z
+
+	public void h()
+	{
+
+		// commentar z
 		System.out.println("z");
-		}
-	public void i() {
-		//commentar i
+
+	}
+
+	public void i()
+	{
+
+		// commentar i
 		System.out.println("i");
+
 	}
-	
-	public void y() {
-		//commentar y
+
+	public void y()
+	{
+
+		// commentar y
 		System.out.println("y");
+
 	}
-	
-	public void o() {
-		//commentar o
+
+	public void o()
+	{
+
+		// commentar o
 		System.out.println("o");
+
 	}
-		
-	public void z() {
-		//commentar z
+
+	public void z()
+	{
+
+		// commentar z
 		System.out.println("z");
+
 	}
-	
-	public void x() {
-		//commentar x
+
+	public void x()
+	{
+
+		// commentar x
 		System.out.println("x123");
 		System.out.println("x");
+
 	}
-	
-	public void w() {
-		//commentar w
+
+	public void w()
+	{
+
+		// commentar w
 		System.out.println("w");
+
 	}
-	
-	public void r() {
-		//commentar r
+
+	public void r()
+	{
+
+		// commentar r
 		System.out.println("r");
+
 	}
-	
-	public void s() {
-		//commentar s
+
+	public void s()
+	{
+
+		// commentar s
 		System.out.println("s");
+
 	}
-	
-	public void q1() {
-		//commentar q1
+
+	public void q1()
+	{
+
+		// commentar q1
 		System.out.println("q1");
+
 	}
-	
-	public void j() {
-		//commentar j
+
+	public void j()
+	{
+
+		// commentar j
 		System.out.println("j");
-		}
-	
-	public void p() {
-		//commentar p 123
-		System.out.println("p");
-		}
-	
-	public void q() {
-		//commentar q
-		System.out.println("q");
+
 	}
-	
-	
-	
-	
+
+	public void p()
+	{
+
+		// commentar p 123
+		System.out.println("p");
+
+	}
+
+	public void qaaa()
+	{
+
+		// commentar qaaa
+		System.out.println("qaaa");
+
+	}
+
+	public void q()
+	{
+
+		// commentar q
+		System.out.println("q");
+
+	}
+
 }
